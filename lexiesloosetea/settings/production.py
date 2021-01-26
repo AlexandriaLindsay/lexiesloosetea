6d4d1d5a7a4803058cc0f14205c7789a5a50d0f3
alex <alex@psone.ca>
2021-01-26 12:07:15 -0600

    dd

--- lexiesloosetea/settings/production.py
+++ lexiesloosetea/settings/production.py
@@ -1,6 +1,33 @@
+import os
 from .base import *
 
 DEBUG = False
+SECRET_KEY = 'i)hncbk8oo2!b_6_b7_ka30&&*#+l76w$c=f!ff@5157)rhz3*'
+ALLOWED_HOSTS = ['localhosts', 'lexiesloosetea.ca', '*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'NAME': 'lexiesloosetea',
+        'USER': 'lexiesloosetea',
+        'PASSWORD': 'residentevil808',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://222d12286c4642299cdad51907ee77f3@o511268.ingest.sentry.io/5608545",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 try:
     from .local import *
